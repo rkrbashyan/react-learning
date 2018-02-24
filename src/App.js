75448b7b46b85988287aa9e3200157ecf4dde3b7
Ruben Krbashyan <krbashyan@gmail.com>
2018-02-24 15:31:51 +0200

    radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
 	state = {
@@ -54,10 +55,15 @@ class App extends Component {
 
 	render() {
 		const style = {
-			backgroundColor: 'white',
+			backgroundColor: 'green',
+			color: 'white',
 			border: '1px solid blue',
 			padding: '16px',
-			cursor: 'pointer'
+			cursor: 'pointer',
+			':hover': {
+				backgroundColor: 'lightgreen',
+				color: 'black'
+			}
 		};
 
 		let persons = null;
@@ -78,22 +84,41 @@ class App extends Component {
 					))}
 				</div>
 			);
+
+			style.backgroundColor = 'red';
+			style[':hover'] = {
+				backgroundColor: 'salmon',
+				color: 'black'
+			};
+		}
+
+		const classes = [];
+
+		if (this.state.persons.length <= 2) {
+			classes.push('red');
+		}
+
+		if (this.state.persons.length <= 1) {
+			classes.push('bold');
 		}
 
 		return (
-			<div className="App">
-				<h1> Hi, I'm a React App</h1>
-				<button
-					style={style}
-					/* onClick={this.switchNameHandler.bind(this, 'Ruben')} */
-					onClick={this.togglePersonHandler}
-				>
-					Switch Name
-				</button>
-				{persons}
-			</div>
+			<StyleRoot>
+				<div className="App">
+					<h1> Hi, I'm a React App</h1>
+					<p className={classes.join(' ')}>This is really working!</p>
+					<button
+						style={style}
+						/* onClick={this.switchNameHandler.bind(this, 'Ruben')} */
+						onClick={this.togglePersonHandler}
+					>
+						Switch Name
+					</button>
+					{persons}
+				</div>
+			</StyleRoot>
 		);
 	}
 }
 
-export default App;
+export default Radium(App);
