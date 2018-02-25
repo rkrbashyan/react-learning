9c09c5941125af3370272754691fd83cc8709f2f
Ruben Krbashyan <krbashyan@gmail.com>
2018-02-25 15:24:02 +0200

    error-handling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 	state = {
@@ -59,15 +60,16 @@ class App extends Component {
 			persons = (
 				<div>
 					{this.state.persons.map((person, index) => (
-						<Person
-							key={person.id}
-							name={person.name}
-							age={person.age}
-							click={() => this.deletePersonHandler(index)}
-							changed={(event) =>
-								this.nameChangedHandler(event, person.id)
-							}
-						/>
+						<ErrorBoundary key={person.id}>
+							<Person
+								name={person.name}
+								age={person.age}
+								click={() => this.deletePersonHandler(index)}
+								changed={(event) =>
+									this.nameChangedHandler(event, person.id)
+								}
+							/>
+						</ErrorBoundary>
 					))}
 				</div>
 			);
